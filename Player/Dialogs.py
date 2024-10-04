ec340cb7e67fb8059b41ab891a2c0f1fe9e49eff
Kevin <kevinvosburgh4@gmail.com>
2024-10-03 20:25:11 -0400

    Get location of all dialog exits

--- Player/Dialogs.py
+++ Player/Dialogs.py
@@ -3,6 +3,7 @@ from ImageParser.FindBoxMk2 import find_boxes
 from ImageParser.Util import get_box_min_y_start
 from ocr.tess import parseImage, TesseractOption, bestStringMatch
 from enum import Enum
+from typing import Tuple
 
 
 class Dialogs(Enum):
@@ -25,26 +26,51 @@ Dialog_titles = {
     Dialogs.Research: "COMMON",
 }
 
+blue_dialogs = [
+    Dialogs.Shipping,
+    Dialogs.MainMenu,
+    Dialogs.HenHousing,
+    Dialogs.GrainSilos,
+]
+purple_dialogs = [Dialogs.Prestige]
+red_exit_dialogs = [Dialogs.Farm, Dialogs.Research]
+
+purple_header_color = (134, 0, 196)
+blue_header_color = (39, 110, 198)
+red_exit_color = (240, 13, 13)
+
+
+def get_dialog_name(dialog_value):
+    for key in Dialog_titles.keys():
+        if Dialog_titles[key] == dialog_value:
+            return key
+    return None
+
+
+def prep_return_value(result):
+    accuracy, loc = result
+    return get_dialog_name(accuracy[0]), loc
+
 
 def get_visible_dialogs(ti: TransformationImage):
     blue_dialogs = get_visible_blue_dialogs(ti)
-    if blue_dialogs is not None and blue_dialogs[1] > 0.7:
+    if blue_dialogs is not None and blue_dialogs[0][1] > 0.7:
         print("blue")
-        return blue_dialogs
+        return prep_return_value(blue_dialogs)
     blue_small_dialogs = get_visible_small_blue_dialogs(ti)
-    if blue_small_dialogs is not None and blue_small_dialogs[1] > 0.7:
+    if blue_small_dialogs is not None and blue_small_dialogs[0][1] > 0.7:
         print("small blue")
-        return blue_small_dialogs
+        return prep_return_value(blue_small_dialogs)
     purple_dialogs = get_purple_visible_dialogs(ti)
-    if purple_dialogs is not None and purple_dialogs[1] > 0.7:
+    if purple_dialogs is not None and purple_dialogs[0][1] > 0.7:
         print("purple")
-        return purple_dialogs
+        return prep_return_value(purple_dialogs)
     return None
 
 
 def get_visible_blue_dialogs(ti: TransformationImage):
     im = ti.get_pil_image()
-    color = (39, 110, 198)
+    color = blue_header_color
     min_size_x = 2 / 3 * im.size[0]
     min_size_y = 60
     x_step = 4
@@ -81,12 +107,12 @@ def get_visible_blue_dialogs(ti: TransformationImage):
         results.replace("\n", ""), list(Dialog_titles.values())
     )
 
-    return best_match
+    return best_match, locations[-1]
 
 
 def get_visible_small_blue_dialogs(ti: TransformationImage):
     im = ti.get_pil_image()
-    color = (39, 110, 198)
+    color = blue_header_color
     min_size_x = im.size[0] // 4
     min_size_y = 60
     x_step = 10
@@ -125,12 +151,12 @@ def get_visible_small_blue_dialogs(ti: TransformationImage):
         results.replace("\n", ""), list(Dialog_titles.values())
     )
 
-    return best_match
+    return best_match, chosen_location
 
 
 def get_purple_visible_dialogs(ti: TransformationImage):
     im = ti.get_pil_image()
-    color = (134, 0, 196)
+    color = purple_header_color
     min_size_x = 2 / 3 * im.size[0]
     min_size_y = 60
     x_step = 4
@@ -167,4 +193,40 @@ def get_purple_visible_dialogs(ti: TransformationImage):
         results.replace("\n", ""), list(Dialog_titles.values())
     )
 
-    return best_match
+    return best_match, locations[-1]
+
+
+def get_dialog_close(
+    ti: TransformationImage,
+    dialog_name: Dialogs,
+    dialog_title_location: Tuple[int, int, int, int],
+):
+    im = ti.get_pil_image()
+    color = blue_header_color
+    if dialog_name in purple_dialogs:
+        color = purple_header_color
+    elif dialog_name in red_exit_dialogs:
+        color = red_exit_color
+    min_size_x = 10
+    min_size_y = 10
+    x_step = 1
+    y_step = 1
+    start_x = (im.width * 3) // 4
+    start_y = dialog_title_location[1] - 20
+    end_x = im.width
+    end_y = dialog_title_location[3] + 20
+
+    locations = find_boxes(
+        im,
+        color,
+        min_size_x,
+        min_size_y,
+        x_step,
+        y_step,
+        start_x,
+        start_y,
+        end_x,
+        end_y,
+    )
+
+    return locations
